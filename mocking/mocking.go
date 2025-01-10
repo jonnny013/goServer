2d7b40d702ff7c0ad05543185b5e105372ce5473
Jon Love <jon_w-l@hotmail.com>
2025-01-10 17:02:50 +0800

    Finish mocking. Review later**

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -1,3 +1,7 @@
+// Not really clear on this whole topic. Come back another day
+// https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/mocking
+
+
 package main
 
 import (
@@ -18,7 +22,28 @@ type SpySleeper struct {
 	Calls int
 }
 
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
 type DefaultSleeper struct{}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
 
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
@@ -27,6 +52,18 @@ func (d *DefaultSleeper) Sleep() {
 func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
 
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
@@ -37,6 +74,9 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{
+		2 * time.Second,
+		time.Sleep,
+	}
 	Countdown(os.Stdout, sleeper)
 }
