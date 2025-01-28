e329875bb847d0231e2a91d30e0b93ebb911f45b
Jon Love <jon_w-l@hotmail.com>
2025-01-28 15:24:38 +0800

    Finished this chapter

--- blog_files/readFile_test.go
+++ blog_files/readFile_test.go
@@ -2,20 +2,47 @@ package blogfiles_test
 
 import (
 	blogfiles "go-server/blog_files"
-	"testing/fstest"
+	"reflect"
 	"testing"
+	"testing/fstest"
 )
 
-
 func TestNewBlogPosts(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1
+Tags: tdd, go
+---
+Hello
+world!
+`
+		secondBody = `Title: Post 2
+Description: Description 2
+Tags: rust, borrow-checker
+---
+B
+L
+M`
+	)
+
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
+	}
+	posts, err := blogfiles.NewPostsFromFS(fs)
+
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	posts := blogfiles.NewPostsFromFS(fs)
+	assertPost(t, posts[0], blogfiles.Post{Title: "Post 1",
+		Description: "Description 1", Tags: []string{"tdd", "go"}, Body: `Hello
+world!`})
+}
 
-	if len(posts) != len(fs) {
-		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+func assertPost(t *testing.T, got blogfiles.Post, want blogfiles.Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
 	}
 }
