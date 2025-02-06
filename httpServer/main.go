9fbf5c81041108f75962148ada97857a3966bad9
Jon Love <jon_w-l@hotmail.com>
2025-02-06 21:06:40 +0800

    Fix filing systme and package issues while continuing to work on the server

--- httpServer/main.go
+++ httpServer/main.go
@@ -6,9 +6,17 @@ import (
 	"net/http"
 )
 
+
+
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
-	handler := http.HandlerFunc(PlayerServer)
+	server := &PlayerServer{&InMemoryPlayerStore{}}
 	port := 8080
 	fmt.Printf("Server running on %d", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), handler))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), server))
 }
