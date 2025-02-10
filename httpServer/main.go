640127e7a8d186f1267caddbfc0c750d15039c52
Jon Love <jon_w-l@hotmail.com>
2025-02-10 21:07:00 +0800

    Update main to use in_memory_player_store

--- httpServer/main.go
+++ httpServer/main.go
@@ -6,20 +6,8 @@ import (
 	"net/http"
 )
 
-
-
-type InMemoryPlayerStore struct{}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-	
-}
-
 func main() {
-	server := &PlayerServer{&InMemoryPlayerStore{}}
+	server := &PlayerServer{NewInMemoryPlayerStore()}
 	port := 8080
 	fmt.Printf("Server running on %d", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), server))
