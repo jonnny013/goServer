571494a7dd250df2b5f2200e39cb5188ba05766f
Jon Love <jon_w-l@hotmail.com>
2025-02-10 20:55:45 +0800

    Make updates

--- httpServer/server.go
+++ httpServer/server.go
@@ -16,17 +16,16 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
+	player := getPlayerName(r)
 	switch r.Method {
 	case http.MethodPost:
-		p.processWin(w)
+		p.processWin(w, player)
 	case http.MethodGet:
-		p.showScore(w, r)
+		p.showScore(w, player)
 	}
 }
 
-func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
 
 	score := p.store.GetPlayerScore(player)
 
@@ -37,8 +36,14 @@ func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, score)
 }
 
-func (p *PlayerServer) processWin(w http.ResponseWriter) {
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+	p.store.RecordWin(player)
 	w.WriteHeader(http.StatusAccepted)
+}
+
+func getPlayerName(r *http.Request) string {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	return player
 }
 
 func GetPlayerScore(name string) string {
